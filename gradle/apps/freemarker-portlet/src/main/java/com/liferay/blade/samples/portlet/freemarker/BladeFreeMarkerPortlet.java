2269122e5199825bf089b4ef9d3a72bb96162786
Tamas Molnar <tamas.molnar@liferay.com>
2018-04-04 11:20:52 +0200

    LPP-29498 Copy include method from FreeMarkerPortlet and add template.prepare(PortalUtil.getHttpServletRequest(portletRequest)) call

--- gradle/apps/freemarker-portlet/src/main/java/com/liferay/blade/samples/portlet/freemarker/BladeFreeMarkerPortlet.java
+++ gradle/apps/freemarker-portlet/src/main/java/com/liferay/blade/samples/portlet/freemarker/BladeFreeMarkerPortlet.java
@@ -16,9 +16,27 @@
 
 package com.liferay.blade.samples.portlet.freemarker;
 
+import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.template.Template;
+import com.liferay.portal.kernel.template.TemplateConstants;
+import com.liferay.portal.kernel.template.TemplateException;
+import com.liferay.portal.kernel.template.TemplateManager;
+import com.liferay.portal.kernel.template.TemplateManagerUtil;
+import com.liferay.portal.kernel.template.TemplateResource;
+import com.liferay.portal.kernel.template.TemplateResourceLoaderUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.UnsyncPrintWriterPool;
 import com.liferay.util.bridges.freemarker.FreeMarkerPortlet;
 
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.portlet.MimeResponse;
 import javax.portlet.Portlet;
+import javax.portlet.PortletContext;
+import javax.portlet.PortletException;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -41,4 +59,84 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class BladeFreeMarkerPortlet extends FreeMarkerPortlet {
+
+	@Override
+	protected void include(
+			String path, PortletRequest portletRequest,
+			PortletResponse portletResponse, String lifecycle)
+		throws IOException, PortletException {
+
+		PortletContext portletContext = getPortletContext();
+
+		String servletContextName = portletContext.getPortletContextName();
+
+		String resourcePath = servletContextName.concat(
+			TemplateConstants.SERVLET_SEPARATOR).concat(path);
+
+		boolean resourceExists = false;
+
+		try {
+			resourceExists = TemplateResourceLoaderUtil.hasTemplateResource(
+				TemplateConstants.LANG_TYPE_FTL, resourcePath);
+		}
+		catch (TemplateException te) {
+			throw new IOException(te);
+		}
+
+		if (!resourceExists) {
+
+			// _log.error(path + " is not a valid include");
+
+		}
+		else {
+			try {
+				TemplateResource templateResource =
+					TemplateResourceLoaderUtil.getTemplateResource(
+						TemplateConstants.LANG_TYPE_FTL, resourcePath);
+
+				TemplateManager templateManager =
+					TemplateManagerUtil.getTemplateManager(
+						TemplateConstants.LANG_TYPE_FTL);
+
+				Template template = TemplateManagerUtil.getTemplate(
+					TemplateConstants.LANG_TYPE_FTL, templateResource, false);
+
+				templateManager.addTaglibSupport(
+					template, PortalUtil.getHttpServletRequest(portletRequest),
+					PortalUtil.getHttpServletResponse(portletResponse));
+
+				template.put("portletContext", getPortletContext());
+				template.put(
+					"userInfo",
+					portletRequest.getAttribute(PortletRequest.USER_INFO));
+
+				Writer writer = null;
+
+				if (portletResponse instanceof MimeResponse) {
+					MimeResponse mimeResponse = (MimeResponse)portletResponse;
+
+					writer = UnsyncPrintWriterPool.borrow(
+						mimeResponse.getWriter());
+				}
+				else {
+					writer = new UnsyncStringWriter();
+				}
+
+				template.prepare(
+					PortalUtil.getHttpServletRequest(portletRequest));
+
+				template.processTemplate(writer);
+			}
+			catch (Exception e) {
+				throw new PortletException(e);
+			}
+		}
+
+		if (clearRequestParameters) {
+			if (lifecycle.equals(PortletRequest.RENDER_PHASE)) {
+				portletResponse.setProperty("clear-request-parameters", "true");
+			}
+		}
+	}
+
 }
